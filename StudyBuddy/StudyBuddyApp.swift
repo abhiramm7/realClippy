4cc49faf670eff6af033bbeeb3deafcdc527b94b
Abhi. M <abhirammullapudi@gmail.com>
2025-12-31 01:09:15 -0500

    Initial Commit

---
+++ StudyBuddy/StudyBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyBuddyApp.swift
+//  StudyBuddy
+//
+//  Created by Abhiram Mullapudi on 12/31/25.
+//
+
+import SwiftUI
+
+@main
+struct StudyBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
